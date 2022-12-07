05d611eade93f14a5a1c408cd13a645d0381db8c
Chidimma <chidimmamofodum@gmail.com>
2022-12-07 01:35:56 +0000

    filter: added value attributes to the option tags

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -5,11 +5,11 @@ function Filter() {
     <div>
      <select>
         <option selected>Filter by Region</option>
-        <option>Africa</option>
-        <option>America</option>
-        <option>Asia</option>
-        <option>Europe</option>
-        <option>Oceania</option>
+        <option value= "Africa">Africa</option>
+        <option value = "America">America</option>
+        <option value = "Asia">Asia</option>
+        <option value = "Europe">Europe</option>
+        <option value = "Oceania">Oceania</option>
      </select>
     </div>
   )
